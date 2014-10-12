5236f1dd8df470c8e8d036be2ccb8db34b9546f3
magacoelho <mgacoelho@yahoo.es>
2014-10-13 01:54:17 +0200

    Creacion de Clase Parado y constructor.. metodos sin implementar

---
+++ src/main/java/es/upm/miw/pd/state/connection/Parado.java
@@ -0,0 +1,49 @@
+package es.upm.miw.pd.state.connection;
+
+public class Parado extends EstadoConexion {
+    public Parado(){
+    	estado= Estado.PARADO;
+    }
+	@Override
+	public Estado getEstado(Conexion conexion) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void abrir(Conexion conexion) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void cerrar(Conexion conexion) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void parar(Conexion conexion) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void iniciar(Conexion conexion) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void enviar(Conexion conexion) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void recibir(Conexion conexion) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
