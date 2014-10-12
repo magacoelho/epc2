c75755ac6b0b5df438c113c35ec76ff049c2f157
magacoelho <mgacoelho@yahoo.es>
2014-10-13 00:01:31 +0200

    Metodos de EstadoConexion Agregar metodos asbtractos de estado conexion

--- src/main/java/es/upm/miw/pd/state/connection/EstadoConexion.java
+++ src/main/java/es/upm/miw/pd/state/connection/EstadoConexion.java
@@ -1,5 +1,11 @@
 package es.upm.miw.pd.state.connection;
 
-public class EstadoConexion {
-
+public abstract class EstadoConexion {
+	public abstract Estado getEstado(Conexion conexion); 
+    public abstract void abrir(Conexion conexion); 
+    public abstract void cerrar(Conexion conexion) ;
+    public abstract void parar(Conexion conexion);
+    public abstract void iniciar(Conexion conexion); 
+    public abstract void enviar(Conexion conexion); 
+    public abstract void recibir(Conexion conexion);
 }
