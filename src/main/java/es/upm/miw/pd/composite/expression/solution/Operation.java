60ea4cbab268ff7abc08f8fdbd2624431579e777
magacoelho <mgacoelho@yahoo.es>
2014-10-12 23:06:48 +0200

    Implementación método Sumar.operar

--- src/main/java/es/upm/miw/pd/composite/expression/solution/Operation.java
+++ src/main/java/es/upm/miw/pd/composite/expression/solution/Operation.java
@@ -1,11 +1,11 @@
 package es.upm.miw.pd.composite.expression.solution;
 
 public abstract class Operation extends Expression {
-    private Expression expression1;
-    private Expression expression2;
+    protected Expression expression1;
+    protected Expression expression2;
     protected String sign;
     
-	private Operation(Expression expression1, Expression expression2) {
+	public Operation(Expression expression1, Expression expression2) {
 		super();
 		this.expression1 = expression1;
 		this.expression2 = expression2;
