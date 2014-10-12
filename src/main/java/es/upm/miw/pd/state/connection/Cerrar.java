331bc2eac080388e2019694e1800ec4963203017
magacoelho <mgacoelho@yahoo.es>
2014-10-13 00:03:13 +0200

    Ageragr Clase Cerrar con cabeceras de metodos

---
+++ src/main/java/es/upm/miw/pd/state/connection/Cerrar.java
@@ -0,0 +1,47 @@
+package es.upm.miw.pd.state.connection;
+
+public class Cerrar extends EstadoConexion {
+
+	@Override
+	public Estado getEstado(Conexion conexion) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void abrir(Conexion conexion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cerrar(Conexion conexion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void parar(Conexion conexion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void iniciar(Conexion conexion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void enviar(Conexion conexion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void recibir(Conexion conexion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
