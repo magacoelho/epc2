204442664f68bb050cb2491c71e05e69d7e01f38
magacoelho <mgacoelho@yahoo.es>
2014-10-12 22:31:36 +0200

    Agergacion clase Expression

---
+++ src/main/java/es/upm/miw/pd/composite/expression/solution/Expression.java
@@ -0,0 +1,12 @@
+package es.upm.miw.pd.composite.expression.solution;
+
+public abstract class Expression {
+	public abstract  int operar();
+
+	@Override
+	public String toString(){
+		return this.toString();
+	}
+   
+
+}
