59f4e3c71ba463b1967a1856810c0ce368be1fde
magacoelho <mgacoelho@yahoo.es>
2014-10-12 22:44:51 +0200

    Ahregar clase Operation hereda de Expression Agregar cabecera de métodos abstractos heredados

---
+++ src/main/java/es/upm/miw/pd/composite/expression/solution/Operation.java
@@ -0,0 +1,18 @@
+package es.upm.miw.pd.composite.expression.solution;
+
+public class Operation extends Expression {
+    private Expression expression1;
+    private Expression expression2;
+	@Override
+	public int operar() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
