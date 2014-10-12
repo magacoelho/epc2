50d586e281bd2768dcfda3dcd9257c2f0258adb0
magacoelho <mgacoelho@yahoo.es>
2014-10-12 22:36:30 +0200

    Agregar Clase Numero

---
+++ src/main/java/es/upm/miw/pd/composite/expression/solution/Numero.java
@@ -0,0 +1,21 @@
+package es.upm.miw.pd.composite.expression.solution;
+
+public class Numero extends Expression{
+	private int valor;
+    
+	
+	@Override
+	public int operar() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	  
+
+}
