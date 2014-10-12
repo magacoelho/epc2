e4ed91a3486ce66c9bb9faa79bbc71973fbdd502
magacoelho <mgacoelho@yahoo.es>
2014-10-13 01:53:25 +0200

    Añadir metodos a clase Preparado

---
+++ src/main/java/es/upm/miw/pd/state/connection/Preparado.java
@@ -0,0 +1,47 @@
+package es.upm.miw.pd.state.connection;
+
+public class Preparado extends EstadoConexion {
+    public Preparado(){
+    	estado= Estado.PREPARADO;
+    }
+	@Override
+	public Estado getEstado(Conexion conexion) {
+		return Estado.PREPARADO;
+	}
+
+	@Override
+	public void abrir(Conexion conexion) {
+		conexion.setEstadoConexion(this);
+
+	}
+
+	@Override
+	public void cerrar(Conexion conexion) {
+		conexion.setEstadoConexion(new Cerrado());
+
+	}
+
+	@Override
+	public void parar(Conexion conexion) {
+		conexion.setEstadoConexion(new Parado());
+
+	}
+
+	@Override
+	public void iniciar(Conexion conexion) {
+		conexion.setEstadoConexion(this);
+
+	}
+
+	@Override
+	public void enviar(Conexion conexion) {
+		conexion.setEstadoConexion(new Esperando());
+
+	}
+
+	@Override
+	public void recibir(Conexion conexion) {
+		 throw new UnsupportedOperationException("Acción no permitida... ");
+	}
+
+}
