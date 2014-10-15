3ed3272aadb8cc9cf68e1b1534f36f5282324693
magacoelho <mgacoelho@yahoo.es>
2014-10-15 21:39:46 +0200

    Implementacion del Manager, Crecaion cabecera NNCreator y cabecera metodos

---
+++ src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumberCreator.java
@@ -0,0 +1,10 @@
+package es.upm.miw.pd.factoryMethod.naturalNumber;
+
+public class NaturalNumberCreator {
+
+	public NaturalNumber create() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
