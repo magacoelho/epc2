2d6d75c9940bfef32c873fbb2f97637cecdc7f43
magacoelho <mgacoelho@yahoo.es>
2014-10-21 00:30:58 +0200

    Add clase MementoCalculadora

---
+++ src/main/java/es/upm/miw/pd/command/calculator/memento/MementoCalculadora.java
@@ -0,0 +1,14 @@
+package es.upm.miw.pd.command.calculator.memento;
+
+public class MementoCalculadora {
+  private int valor;
+
+public MementoCalculadora(int valor) {
+	super();
+	this.valor = valor;
+}
+
+public int getValor() {
+	return valor;
+}
+}
