1326642da77f8d4f40908a0c336f8bd54ecbe28f
magacoelho <mgacoelho@yahoo.es>
2014-10-22 19:55:50 +0200

    add clase Texto

---
+++ src/main/java/es/upm/miw/pd/text/solution/Texto.java
@@ -0,0 +1,24 @@
+package es.upm.miw.pd.text.solution;
+
+public class Texto extends Compuesto{
+
+	@Override
+	public void add(Componente c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isComposite() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String dibujar(boolean b) {
+		// TODO Auto-generated method stub
+		return null;
+		
+	}
+
+}
